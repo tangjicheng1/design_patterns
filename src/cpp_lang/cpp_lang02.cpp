f448c1756c9ed71e43a2b587ede42f3935b06764
jichengtang <jichengtang@tencent.com>
2021-12-14 20:37:11 +0800

    [Refactor] 测试继承类对基类的构造、析构函数的调用顺序

--- src/cpp_lang/cpp_lang02.cpp
+++ src/cpp_lang/cpp_lang02.cpp
@@ -5,7 +5,7 @@
 class Base {
  public:
   virtual void Print() = 0;
-  virtual ~Base() = default;  
+  virtual ~Base() = default;
   // 如果没有这句，编译器有warning
   // -Wdelete-abstract-non-virtual-dtor
 };
@@ -24,10 +24,28 @@ void test01() {
   return;
 }
 
-void test02(){}
+// 测试继承类对基类的构造与析构函数的调用顺序
+class A {
+ public:
+  A() { std::cout << "Call A() " << std::endl; }
+  ~A() { std::cout << "Call ~A() " << std::endl; }
+};
+
+class B : public A {
+ public:
+  B() { std::cout << "Call B() " << std::endl; }
+  ~B() { std::cout << "Call ~B() " << std::endl; }
+};
+
+void test02() {
+  B b;
+  return;
+}
 
 int main() {
+  std::cout << "Test 1: " << std::endl;
   test01();
-
+  std::cout << "Test 2 : " << std::endl;
+  test02();
   return 0;
 }
