f23bd951558932ff48b13a4893c4306f005befae
jichengtang <jichengtang@tencent.com>
2021-12-14 20:31:50 +0800

    [Refactor] 添加静态区大小的解释，与操作系统相关

--- src/cpp_lang/cpp_lang03.cpp
+++ src/cpp_lang/cpp_lang03.cpp
@@ -1,12 +1,17 @@
 // 静态存储区的大小
 #include <iostream>
 
+// 和操作系统相关，BSS字段或DATA字段的大小
+// 未初始化对应BSS字段，已初始化对应DATA字段
+// https://stackoverflow.com/questions/18371584/what-is-the-maximum-size-of-static-array-that-can-be-declared-in-gcc
+
 constexpr int N = 1024 * 1024 * 1024;  // GB
-constexpr int M = 1024;
+constexpr int M = 1;
 
 char arr[M][N];
 
 int main() {
+  // 局部变量，对应stack大小
   for (int j = 0; j < M; j++) {
     for (int i = 0; i < N; i++) {
       if (arr[j][i] != 0) {
