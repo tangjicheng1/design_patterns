3a0287e665851544d7ac3b66d45611190aedee02
jichengtang <tangjch15@gmail.com>
2022-07-20 14:02:29 +0800

    [Refactor] 修改adl模式代码

--- src/adl.cpp
+++ src/adl.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 
+namespace adl_test {
 namespace a_name {
 struct A {
   A(int val) { x = val; }
@@ -18,12 +19,13 @@ struct A {
 };
 void Print(A a) { std::cout << "b_name::print" << a.x << std::endl; }
 }  // namespace b_name
+}  // namespace adl_test
 
-int main() { 
-  a_name::A a_name_val(1);
-  b_name::A b_name_val(2);
+int main() {
+  adl_test::a_name::A a_name_val(1);
+  adl_test::b_name::A b_name_val(2);
   Print(a_name_val);
   Print(b_name_val);
 
-  return 0; 
+  return 0;
 }
