820982ad90f96eee0d4f49529fec35d13cb9c10e
tangjicheng <tangjicheng@sogou-inc.com>
2022-08-30 19:53:02 +0800

    add lock gaurd test

--- parallel/lock_gaurd_test.cpp
+++ parallel/lock_gaurd_test.cpp
@@ -3,23 +3,29 @@
 #include <thread>
 #include <string>
 #include <vector>
+#include <chrono>
 
-std::mutex m_print;
+std::mutex m_global;
 double global_val = 0.0;
 
 
 void print(int index, std::string str) {
-  std::lock_guard<std::mutex> lock_gaurd_for_print(m_print);
+  // std::lock_guard<std::mutex> lock_gaurd_for_global_val(m_global);
   printf("%d, %s\n", index, str.c_str());
-  global_val = global_val + 1.0;
+  std::chrono::milliseconds dur(10);
+
+  double temp = global_val;
+  std::this_thread::sleep_for(dur);
+  global_val = temp + 1.0;
+
   return; 
 }
 
 void test1() {
   std::vector<std::thread> thread_vec;
-  int thread_num = 32;
+  int thread_num = 10;
   for (int i = 0; i < thread_num; i++) {
-    thread_vec.push_back(std::thread(print, i, std::to_string(i)));
+    thread_vec.push_back(std::thread(print, i, std::to_string(i) + "_thread"));
   }
 
   for (auto& iter : thread_vec) {
