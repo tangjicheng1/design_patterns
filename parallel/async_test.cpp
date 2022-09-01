301e326c2493a8547f81243c0773322b66bedb0d
tangjicheng <tangjicheng@sogou-inc.com>
2022-09-01 17:16:31 +0800

    async blocking

--- parallel/async_test.cpp
+++ parallel/async_test.cpp
@@ -1,22 +1,25 @@
-#include <future>
-#include <iostream>
+#include <chrono>
 #include <cstdio>
 #include <functional>
+#include <future>
+#include <iostream>
 
 int id = 0;
 
 int simple_worker() {
-  id += 1;
-  printf("Calling simple %d\n", id);
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  printf("Calling simple 1\n");
+  return id;
+}
+
+int simple_worker2() {
+  printf("Calling simple 2 \n");
   return id;
 }
 
 void test1() {
-  auto f1 = std::async(std::launch::async, simple_worker);
-  auto f2 = std::async(std::launch::async, simple_worker);
-  
-  std::cout << f1.get() << std::endl;
-  std::cout << f2.get() << std::endl;
+  std::async(std::launch::async, simple_worker);
+  std::async(std::launch::async, simple_worker2);
 }
 
 int main() {
