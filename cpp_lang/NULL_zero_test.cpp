f3a2f308843d22866dc6580aef26b4ada33da8fa
tangjicheng <tangjicheng@sogou-inc.com>
2022-09-02 14:40:06 +0800

    clang decltype(NULL)==long

--- cpp_lang/NULL_zero_test.cpp
+++ cpp_lang/NULL_zero_test.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <type_traits>
 
 void foo(int i) {
   std::cout << "int:" << i << std::endl;
+  return;
 }
 
 void foo(char* str) {
   if (str == nullptr) {
     std::cout << "str: nullptr\n";
+    return;
   }
   std::cout << "str:" << str << std::endl;
 }
@@ -15,6 +18,10 @@ void test() {
   // foo(NULL);
   foo(0);
   foo(nullptr);
+
+  std::cout << std::boolalpha << std::is_same<decltype(nullptr), decltype(NULL)>::value << std::endl;
+  std::cout << std::boolalpha << std::is_same<long, decltype(NULL)>::value << std::endl;
+  std::cout << std::boolalpha << std::is_same<void*, decltype(NULL)>::value << std::endl;
 }
 
 int main() {
