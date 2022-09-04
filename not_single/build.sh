fcf58c9d969413f80976776f3d35a91f69120aac
tangjicheng <tangjicheng@tencent.com>
2022-09-04 21:02:12 +0800

    delete build.sh

--- not_single/build.sh
+++
@@ -1,4 +0,0 @@
-# g++-11 -std=c++17 struct01.cpp -o struct01
-clang++ -std=c++17 struct01.cpp -o struct01
-# gcc-11 编译出错
-# error: no match for 'operator=' (operand types are 'std::tuple<int, int>' and 'std::array<int, 2>')
