340a7c561088ed523d0fe8281a25b31b5fe9d18b
tangjicheng <tangjicheng@sogou-inc.com>
2022-09-26 19:59:18 +0800

    添加json测试

---
+++ json_test/json_test1.cpp
@@ -0,0 +1,14 @@
+#include "nlohmann/json.hpp"
+#include <iostream>
+
+using json = nlohmann::json;
+
+void test1() {
+  json j = {{"one", 1}, {"two",2}};
+  std::cout << j.size() << std::endl;
+}
+
+int main() {
+  test1();
+  return 0;
+}
