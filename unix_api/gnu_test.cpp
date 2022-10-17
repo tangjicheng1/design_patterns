e61eacaba6ec39f5f6a5c2d0b9438f1896aa8272
jichengtang <jichengtang@tencent.com>
2022-10-17 11:27:23 +0800

    add gnu test

---
+++ unix_api/gnu_test.cpp
@@ -0,0 +1,24 @@
+#ifdef __GNUC__
+#  include <gnu/libc-version.h>
+
+#  include <iostream>
+int test(int argc, char** argv) {
+  std::cout << "GNU GLIBC version: " << gnu_get_libc_version() << std::endl;
+  std::cout << "GNU GLIBC release: " << gnu_get_libc_release() << std::endl;
+
+#  if defined(__linux)
+  std::cout << "Linux: " << __linux << std::endl;
+#  endif
+
+  return 0;
+}
+
+#endif
+
+int main(int argc, char** argv) {
+#ifdef __GNUC__
+  return test(argc, argv);
+#else
+  return 0;
+#endif
+}
