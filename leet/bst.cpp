f72083f44689eb850eb9a656ca06bb3877b7b88d
jichengtang <jichengtang@tencent.com>
2022-11-21 11:38:05 +0800

    add binary search tree

---
+++ leet/bst.cpp
@@ -0,0 +1,16 @@
+// 二叉搜索树
+// Binary Search Tree
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// TODO, build, search, add, delete
