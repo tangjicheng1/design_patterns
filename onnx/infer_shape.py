e0ac3b09bfa09d07ebe0cb26ac3d2b8e6de328e0
tangjicheng <tangjicheng@sogou-inc.com>
2022-12-06 14:48:55 +0800

    fix infer_shape.py

--- onnx/infer_shape.py
+++ onnx/infer_shape.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
   input_name = sys.argv[1]
   output_name = sys.argv[2]
   input_model = onnx.load(input_name)
-  output_model = onnx.shape_inferece.infer_shapes(input_model)
+  output_model = onnx.shape_inference.infer_shapes(input_model)
   onnx.save(output_model, output_name)
   # onnx.save(onnx.shape_inference.infer_shapes(onnx.load(input_name)), output_name)
